271d8f3fbf38b5049f0e7a049f854df6067ed1c1
cmvyas <chandramani09022001@gmail.com>
2021-07-19 18:20:24 +0530

    db

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fileUpload = require("express-fileupload");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
